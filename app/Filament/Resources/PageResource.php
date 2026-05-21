32843cd771a43429cca3f8fe1a8490e599a50434
Takwa Nassar <takwanassar1@gmail.com>
2026-05-21 16:14:46 +0300

    updates

--- app/Filament/Resources/PageResource.php
+++ app/Filament/Resources/PageResource.php
@@ -124,12 +124,12 @@ class PageResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
             ]);
+            //->bulkActions([
+           //     Tables\Actions\BulkActionGroup::make([
+            //        Tables\Actions\DeleteBulkAction::make(),
+            //    ]),
+            //]);
     }
 
     public static function getRelations(): array
